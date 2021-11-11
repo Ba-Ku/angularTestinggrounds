eb7ce20426a7b82175985e59a934ae89d61727cd
Ba-Ku <90610126+Ba-Ku@users.noreply.github.com>
2021-11-11 15:42:39 +0100

    ------------------------------------------------------------ angularTestinggrounds is a project to experiment with all things angular. ------------------------------------------------------------ - added pipe moudule - added primeNg modules (table / charts) - added countdown component - added mandatory dirctive - created a person pipe - created an imput directive - created a fetch method (not a module) - created a type.ts file for a person class - created a databasedummy (array) to test tables - debugged all things above ----------------------------------------------- - added two countdowns -----------------------------------------------

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import {person} from "./types";
 
 const PERSONS: person[] = [
   {firstName: 'Holly', lastName: 'Wood', title: 'Mr', age: 70},
-  {firstName: 'Random', lastName: 'User', title:'X', age: 55},
+  {firstName: 'Random', lastName: 'User', title: 'X', age: 55},
   new person('Martina', 'Musterfrau', 'Mrs', 29)
 ];
 
@@ -28,25 +28,21 @@ const PERSONS: person[] = [
     <br>
     <br>
     <div>
-        <ul>
-          <li *ngFor="let givenUser of givenUsers">
-            {{givenUser | person: true :true :false}}</li>
-        </ul>
+      <ul>
+        <li *ngFor="let givenUser of givenUsers">
+          {{givenUser | person: true :true :false}}</li>
+      </ul>
     </div>
     <br>
     <br>
-    <!--
-    <button (click)="fetchDataForPipeExercise()">FETCH FOR PIPERESULTS!</button>
-    <br>
-    <br>
-    <p-tabView>
-      <p-tabPanel *ngFor="let transformedUser of usersForPipe">
-        {{transformedUser | person: true :true :true}}
-      </p-tabPanel>
-    </p-tabView> -->
-    <br>
-    <br>
     <app-countdown></app-countdown>
+    <br>
+    <app-countdown
+      [time]="15"
+      buttonLabel="Second Countdown"
+      (timeChange)="countdownTimeChanged($event)"
+      (finish)="finishCountdown()"
+    ></app-countdown>
   `,
   styles: []
 })
@@ -54,7 +50,6 @@ export class AppComponent {
   users;//u.U noch einen check ob users.length > 0 - sonst ausgabe "no users found"
   inputNumberOfPersonsGenerated = 0;
   givenUsers = PERSONS;
-  //usersForPipe;
 
   checkInCaseFetchData(): void {
     if (this.inputNumberOfPersonsGenerated > 0) {
@@ -73,34 +68,9 @@ export class AppComponent {
           .filter(user => user.gender === 'female')
           .map(user => user.name.title + ' ' + user.name.first + ' ' + user.name.last)
         //.join(', ') -> würde alles zu einem datensatzstring machen
-        console.log(femaleUsers);
         this.users = femaleUsers;
-      })
+      });
   }
-
-  /*fetchDataForPipeExercise(): any{
-    fetch('https://randomuser.me/api?results=' + this.inputNumberOfPersonsGenerated)
-      .then(response => response.json())
-      .then(data => data.results)
-      .then(users => {
-        const femaleUsers = users
-          .filter(user => user.gender === 'female')
-        this.usersForPipe = femaleUsers;
-      })
-    this.transformUsersForPipe()
-  }
-
-  transformUsersForPipe():void{
-    const transferArray: person[] = [];
-    for(let user of this.usersForPipe){
-      let transformedUser = new person(user.name.first, user.name.last, user.name.title, user.dob.age)
-      //{firstName: user.name.first, lastName: user.name.last, title: user.name.title, age: user.dob.age}
-      transferArray.push(transformedUser);
-    }
-    console.log(transferArray);
-    this.usersForPipe = transferArray;
-    console.log(this.usersForPipe);
-  }*/
 
   incrementInputNumberOfPersonsGenerated() {
     this.inputNumberOfPersonsGenerated++;
@@ -108,5 +78,13 @@ export class AppComponent {
 
   decrementInputNumberOfPersonsGenerated() {
     this.inputNumberOfPersonsGenerated--;
+  }
+
+  countdownTimeChanged(time: number): void {
+    console.log('Time from second countdown: ' + time);
+  }
+
+  finishCountdown():void{
+    console.log('second countdown finished!');
   }
 }
--- src/app/countdown.component.ts
+++ src/app/countdown.component.ts
@@ -1,20 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {timer} from "rxjs";
 
 @Component({
   selector: 'app-countdown',
   template: `
-    <p>
-      countdown works!
-    </p>
+    <h4 class="center">Countdown!</h4>
+   <div class="center">
+     <input type="number" [(ngModel)]="time">
+     <button (click)="startCountdown()">{{buttonLabel}}</button>
+   </div>
+   <div class="center" *ngIf="running==true">
+     <p>Output: {{time}}</p>
+   </div>
   `,
   styles: [
   ]
 })
-export class CountdownComponent implements OnInit {
+export class CountdownComponent{
 
   constructor() { }
 
-  ngOnInit(): void {
+  @Input() time=10;
+  @Input() buttonLabel ='Start countdown!'
+  running = false;
+  @Output() timeChange = new EventEmitter<number>();
+  @Output() finish = new EventEmitter;
+
+  startCountdown():void{
+    this.running = true;
+    const startingTime = this.time;
+    const subscription = timer(1000, 1000).subscribe(tick =>{
+      this.time --;
+      this.timeChange.emit(this.time);
+
+      if(this.time <=0){
+        subscription.unsubscribe();
+        this.running = false;
+        this.finish.emit();
+        this.time = startingTime;
+      }
+    });
   }
 
 }
